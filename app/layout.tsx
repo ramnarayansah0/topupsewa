cff48409780c8fbd3eb7b1cbc79700bde234b85f
Ramnarayan <ramnarayansah180@gmail.com>
2025-03-21 17:04:45 +0545

    this is update

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
@@ -29,7 +30,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
-       
+        <SpeedInsights />
       </body>
     </html>
   );
